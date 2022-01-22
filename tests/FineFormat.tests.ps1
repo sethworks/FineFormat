8b57f7a8d745aff2c01158956725c8c20c044ff6
sethvs <sethvs@users.noreply.github.com>
2022-01-22 13:59:04 +0300

    Initial tests

---
+++ tests/FineFormat.tests.ps1
@@ -0,0 +1,8 @@
+BeforeAll {
+    Remove-Module -Name FineFormat -Force -ErrorAction SilentlyContinue
+    Import-Module -Name "$PSScriptRoot\..\FineFormat.psd1"
+}
+
+Describe "FineFormat" {
+    
+}
---
+++ tests/PesterConfiguration.CodeCoverage.ps1
@@ -0,0 +1,2 @@
+$PesterConfiguration.CodeCoverage.Enabled = $true
+$PesterConfiguration.CodeCoverage.Path = 'FineFormat.functions.ps1'
