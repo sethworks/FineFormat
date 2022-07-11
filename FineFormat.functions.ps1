8b0a3ba61a45e7818a95f74bc909ade4beffe33b
sethvs <sethvs@users.noreply.github.com>
2022-07-11 08:15:29 +0300

    Cleanup

--- FineFormat.functions.ps1
+++ FineFormat.functions.ps1
@@ -35,73 +35,9 @@ function Format-Fine
                     continue
                 }
 
-<#
-                # NotNullOrEmpty
-                if ($NotNullOrEmpty -and ([string]::IsNullOrEmpty($p.Value)))
-                {
-                    continue
-                }
-
-                # NullOrEmpty
-                if ($NullOrEmpty -and -not [string]::IsNullOrEmpty($p.Value))
-                {
-                    continue
-                }
-
-                # Numeric
-                if ($Numeric -and $p.TypeNameOfValue -notmatch '^System\.(U)?Int(\d\d)?$|^System\.Single$|^System\.Double$|^System\.Decimal$|^(u)?short$|^(u)?int$|^(u)?long$')
-                {
-                    continue
-                }
-#>
-
                 $hash.Add($p.Name, $p.Value)
             }
             [PSCustomObject]$hash
-
-<#
-            # NotNullOrEmpty
-            if ($NotNullOrEmpty)
-            {
-                $hash = [ordered]@{}
-                foreach ($p in $io.PSObject.Properties)
-                {
-                    if ( -not [string]::IsNullOrEmpty($p.Value) )
-                    {
-                        $hash.Add($p.Name, $p.Value)
-                    }
-                }
-                [PSCustomObject]$hash
-            }
-
-            # NullOrEmpty
-            if ($NullOrEmpty)
-            {
-                $hash = [ordered]@{}
-                foreach ($p in $io.PSObject.Properties)
-                {
-                    if ( [string]::IsNullOrEmpty($p.Value) )
-                    {
-                        $hash.Add($p.Name, $p.Value)
-                    }
-                }
-                [PSCustomObject]$hash
-            }
-
-            # Numeric
-            if ($Numeric)
-            {
-                $hash = [ordered]@{}
-                foreach ($p in $io.PSObject.Properties)
-                {
-                    if ($p.TypeNameOfValue -match '^System\.(U)?Int(\d\d)?$|^System\.Single$|^System\.Double$|^System\.Decimal$|^(u)?short$|^(u)?int$|^(u)?long$')
-                    {
-                        $hash.Add($p.Name, $p.Value)
-                    }
-                }
-                [PSCustomObject]$hash
-            }
-#>
         }
     }
 }
