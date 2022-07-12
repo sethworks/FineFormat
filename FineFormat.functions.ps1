3223875a5fa72d9b20dba29562b460efab21a276
sethvs <sethvs@users.noreply.github.com>
2022-07-12 11:20:33 +0300

    Add -CompactNumbers parameter

--- FineFormat.functions.ps1
+++ FineFormat.functions.ps1
@@ -1,20 +1,12 @@
-enum prefix
-{
-    KB = 0
-    MB = 1
-    GB = 2
-    TB = 3
-    PB = 4
-}
-
-$NumbersAsValues = @('KB', 'MB', 'GB', 'TB', 'PB', 'Max')
+$NumbersAsValues = @('KB', 'MB', 'GB', 'TB', 'PB')
 function Format-Fine
 {
     Param(
         [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
         $InputObject,
-        [Alias('HaveValue')]
-        [switch]$NotNullOrEmpty,
+        [Alias('NotNullOrEmpty')]
+        [switch]$HaveValue,
+        [switch]$CompactNumbers,
         [switch]$NullOrEmpty,
         [switch]$Numeric,
         [switch]$Textual,
@@ -23,14 +15,14 @@ function Format-Fine
         [switch]$NumberGroupSeparator,
         # [ValidateSet('KB', 'MB', 'GB', 'TB', 'PB')]
         # [ArgumentCompletions($NumbersAsValues)]
-        [ArgumentCompletions('KB', 'MB', 'GB', 'TB', 'PB', 'Max')]
+        [ArgumentCompletions('KB', 'MB', 'GB', 'TB', 'PB')]
         [string]$NumbersAs
     )
     begin
     {
         if ($NumbersAs -and $NumbersAs -notin $NumbersAsValues)
         {
-            Write-Verbose -Message "-NumbersAs parameter accepts only 'KB', 'MB', 'GB', 'TB', 'PB', or 'Max' values." -Verbose
+            Write-Verbose -Message "-NumbersAs parameter accepts only 'KB', 'MB', 'GB', 'TB', or 'PB' values." -Verbose
         }
     }
     process
@@ -38,7 +30,7 @@ function Format-Fine
         foreach ($io in $InputObject)
         {
             # default
-            if (-not $NotNullOrEmpty -and -not $NullOrEmpty -and -not $Numeric -and -not $Textual -and -not $ValueFilter -and -not $TypeNameFilter -and -not $NumberGroupSeparator -and -not $NumbersAs)
+            if (-not $HaveValue -and -not $CompactNumbers -and -not $NullOrEmpty -and -not $Numeric -and -not $Textual -and -not $ValueFilter -and -not $TypeNameFilter -and -not $NumberGroupSeparator -and -not $NumbersAs)
             {
                 $io
                 continue
@@ -47,7 +39,7 @@ function Format-Fine
             $hash = [ordered]@{}
             foreach ($p in $io.PSObject.Properties)
             {
-                if ( ($NotNullOrEmpty -and ([string]::IsNullOrEmpty($p.Value))) -or
+                if ( ($HaveValue -and ([string]::IsNullOrEmpty($p.Value))) -or
 
                      ($NullOrEmpty -and -not [string]::IsNullOrEmpty($p.Value)) -or
 
@@ -68,12 +60,32 @@ function Format-Fine
                 {
                     $template = "{0:#,0.##}"
                 }
-                elseif ($NumbersAs)
+                elseif ($NumbersAs -or $CompactNumbers)
                 {
                     $template = "{0:0.##}"
                 }
 
-                if ($NumbersAs -in @('KB', 'MB', 'GB', 'TB', 'PB', 'Max') -and $p.TypeNameOfValue -match '^System\.(U)?Int(\d\d)?$|^System\.Single$|^System\.Double$|^System\.Decimal$|^(u)?short$|^(u)?int$|^(u)?long$')
+                if ($CompactNumbers)
+                {
+                    $value = $p.Value
+                    if ([Math]::Floor($value / 1KB))
+                    {
+                        $value /= 1KB
+                        $i = 0
+                        while ([Math]::Floor($value / 1KB))
+                        {
+                            $value /= 1KB
+                            $i++
+                            if ($i -eq 4)
+                            {
+                                break
+                            }
+                        }
+                        $template += " $($NumbersAsValues[$i])"
+                    }
+                    $hash.Add($p.Name, $template -f $value)
+                }
+                elseif ($NumbersAs -in $NumbersAsValues -and $p.TypeNameOfValue -match '^System\.(U)?Int(\d\d)?$|^System\.Single$|^System\.Double$|^System\.Decimal$|^(u)?short$|^(u)?int$|^(u)?long$')
                 {
                     $value = $p.Value
 
@@ -101,21 +113,6 @@ function Format-Fine
                     {
                         $template += " PB"
                         $value /= 1PB
-                    }
-                    elseif ($NumbersAs -eq 'Max')
-                    {
-                        if ([Math]::Floor($value / 1KB))
-                        {
-                            $value /= 1KB
-                            $i = 0
-
-                            while ([Math]::Floor($value / 1KB))
-                            {
-                                $value /= 1KB
-                                $i++
-                            }
-                            $template += " $($NumbersAsValues[$i])"
-                        }
                     }
                     $hash.Add($p.Name, $template -f $value)
 
