56c1c31715aa1df293e2c2cb1029ad5624b7331c
sethvs <sethvs@users.noreply.github.com>
2022-08-30 21:11:59 +0300

    Update tests

--- tests/PesterConfiguration.CodeCoverage.ps1
+++ tests/PesterConfiguration.CodeCoverage.ps1
@@ -1,2 +1,2 @@
 $PesterConfiguration.CodeCoverage.Enabled = $true
-$PesterConfiguration.CodeCoverage.Path = 'FineFormat.functions.ps1'
+$PesterConfiguration.CodeCoverage.Path = 'FineFormat.functions.ps1', 'FineFormat.completers.ps1'
